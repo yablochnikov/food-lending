9d2b69b7aad33f380c1660d22f32d5a64e67f86d
Nikita Yablochnikov <yablochnikovnikita@gmail.com>
2022-11-21 20:14:06 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+"use strict";
+require("es6-promise").polyfill();
+import "nodelist-foreach-polyfill";
+
+import tabs from "./modules/tabs";
+import modal from "./modules/modal";
+import timer from "./modules/timer";
+import cards from "./modules/cards";
+import slider from "./modules/slider";
+import forms from "./modules/forms";
+import calculator from "./modules/calculator";
+import { openModal } from "./modules/modal";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const modalTimerId = setTimeout(
+    () => openModal(".modal", modalTimerId),
+    50000
+  );
+  tabs(
+    ".tabheader__item",
+    ".tabcontent",
+    ".tabheader__items",
+    "tabheader__item_active"
+  );
+  modal("[data-modal]", ".modal", modalTimerId);
+  timer(".timer", "2022-08-14");
+  cards();
+  slider({
+    container: ".offer__slider",
+    slide: ".offer__slide",
+    nextArrow: ".offer__slider-next",
+    prevArrow: ".offer__slider-prev",
+    totalCounter: "#total",
+    currentCounter: "#current",
+    wrapper: ".offer__slider-wrapper",
+    field: ".offer__slider-inner"
+  });
+  forms("form", modalTimerId);
+  calculator();
+});
